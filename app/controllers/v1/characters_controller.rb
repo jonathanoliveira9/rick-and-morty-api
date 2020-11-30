437f29f8de669e242b287e33af5aa60b77327a41
Daniel Machado <danielmachadopintos@gmail.com>
2020-11-30 20:12:35 -0300

    feat(PROD-001): add character controller and api controller

---
+++ app/controllers/v1/characters_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# main controller
+class Api::V1::CharactersController < Api::V1::ApiController
+  before_action :set_character, only: %i[show appears]
+
+  def show
+    @character ? api_success(@character) : api_not_found
+  end
+
+  def character_params
+    params.permit(:id)
+  end
+
+  private
+
+  def set_character
+    @character = by_id('character', character_params[:id] || character_params[:character_id])
+    return api_not_found('não foi encontrado') if @character['error'].present?
+
+    @character
+  end
+end

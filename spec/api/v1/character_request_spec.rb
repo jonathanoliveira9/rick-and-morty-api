a7d44b97bc1e7d986faf07d728bcc80cc43f60ec
Daniel Machado <danielmachadopintos@gmail.com>
2020-11-30 20:36:13 -0300

    feat(PROD-002): add character matcher

--- spec/api/v1/character_request_spec.rb
+++ spec/api/v1/character_request_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Characters', type: :request do
-  context '#appears' do
-    it 'quando achar' do
-      get '/api/v1/characters/1/appears'
-      expect(response).to have_http_status(:ok)
+  describe '#show' do
+    context 'quando achar' do
+      before do
+        get '/api/v1/characters/1'
+      end
+
+      it 'verificar o corpo' do
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to match_json_schema('character')
+      end
     end
   end
 end
